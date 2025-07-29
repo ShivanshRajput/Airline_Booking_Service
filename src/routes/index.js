88a24010ab6f75ab75a1ca1f0b34b5e4f7915774
ShivanshRajput <shivansh.2022ug1003@iiitranchi.ac.in>
2025-07-30 01:13:27 +0530

    First commit

---
+++ src/routes/index.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const router = express.router();
+
+const v1ApiRoutes = require('./v1/index');
+
+router.use('/v1',v1ApiRoutes);
+
+module.exports = router;
---
+++ src/routes/v1/index.js
@@ -0,0 +1,8 @@
+const express = require('express');
+
+const router = express.Router();
+
+// yaha api routes likhne hain 
+
+
+module.exports = router;
